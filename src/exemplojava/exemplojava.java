0490ea9b36122f09ebc1131fe2f2d9fc297d2682
kauan-gvq <kauan.gallatk@gmail.com>
2019-06-25 12:19:42 -0300

    exemplos1

---
+++ src/exemplojava/exemplojava.java
@@ -0,0 +1,16 @@
+package exemplojava;
+
+public class exemplojava {
+   public static void main (String[]args) {
+	   char a = 'a';
+	   int i = 0 ;
+	   float f = 10.3f;
+	   double d = 10;
+	   
+	   //TODO Auto-generated method stub
+	   System.out.println("a =" +a);
+	   System.out.println("i =" +i);
+	   System.out.println("f =" +f);
+	   System.out.println("d =" +d);
+   }
+}
